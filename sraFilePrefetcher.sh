acd5ffb8e8544c1ef41a328b213c163695ed0a9e
pblaney <patrick.blaney@nyulangone.org>
2022-06-16 16:41:07 -0400

    testing issues with grabbing failed downloads

--- sraFilePrefetcher.sh
+++ sraFilePrefetcher.sh
@@ -108,8 +108,8 @@ sraPrefetch
 sleep 7
 
 totalSraAccession=$(cat ${sraListFile} | wc -l)
-successfulDownloads=$(grep -E " was downloaded successfully" sraPrefetch-"${jobName}".log | wc -l)
-#failedDownloads=$(grep "failed to download" sraPrefetch-"${jobName}".log | wc -l)
+successfulDownloads=$(grep -E "SRR.*[1-9]' was downloaded successfully" sraPrefetch-"${jobName}".log | wc -l)
+failedDownloads=$(grep "failed to download" sraPrefetch-"${jobName}".log | wc -l)
 
 #if [[ ${failedDownloads} != 0 ]]; then
 #	grep "failed to download" sraPrefetch-"${jobName}".log | sed -e 's|.*failed to download ||' > failedDownloads-"${jobName}".err
