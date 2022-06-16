0d6867fa0b68a319c7a9fa36d3c2d162fd2779d0
pblaney <patrick.blaney@nyulangone.org>
2022-06-16 17:04:29 -0400

    testing issues with grabbing failed downloads

--- sraFilePrefetcher.sh
+++ sraFilePrefetcher.sh
@@ -111,23 +111,22 @@ totalSraAccession=$(cat ${sraListFile} | wc -l)
 successfulDownloads=$(grep -E "SRR.*[1-9]' was downloaded successfully" sraPrefetch-"${jobName}".log | wc -l)
 
 if [[ ${successfulDownloads} == ${totalSraAccession} ]]; then
-	echo "GOOD"
+	echo 
+	echo "Total       ===> ${totalSraAccession}"
+	echo 
+	echo "Successful  ===> ${successfulDownloads}"
+else
+	failedDownloads=$(grep "failed to download" sraPrefetch-"${jobName}".log | wc -l)
+	grep "failed to download" sraPrefetch-"${jobName}".log | sed -e 's|.*failed to download ||' > failedDownloads-"${jobName}".err
+
+	echo 
+	echo "Total       ===> ${totalSraAccession}"
+	echo 
+	echo "Successful  ===> ${successfulDownloads}"
+	echo 
+	echo "Failed      ===> ${failedDownloads}"
 fi
 
-#failedDownloads=$(grep "failed to download" sraPrefetch-"${jobName}".log | wc -l)
-
-#if [[ ${failedDownloads} != 0 ]]; then
-#	grep "failed to download" sraPrefetch-"${jobName}".log | sed -e 's|.*failed to download ||' > failedDownloads-"${jobName}".err
-#fi
-
-echo 
-echo "###########################################################"
-echo 
-echo "Total       ===> ${totalSraAccession}"
-echo 
-echo "Successful  ===> ${successfulDownloads}"
-echo 
-#echo "Failed      ===> ${failedDownloads}"
 echo 
 echo "###########################################################"
 echo 
